eeadbf9ecd1bf53f748a44b3fd8222adbcdadfea
beroder <beroder@hotmail.com>
2018-05-26 10:16:12 -0400

    update wording

--- 400BadRequest/Program.cs
+++ 400BadRequest/Program.cs
@@ -31,6 +31,7 @@ namespace _400BadRequest
                     Console.WriteLine("Uploading to Blob storage as blob '{0}'", Path.GetFileName(fs.Name));
                     CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(Path.GetFileName(fs.Name));
                     cloudBlockBlob.UploadFromStream(fs);
+                    //show upload completed
                     Console.WriteLine("Upload completed on {0} UTC", DateTime.Now.ToUniversalTime().ToString());
                 }
             }
